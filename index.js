7ece5696570ceeea91a2de533f32e89ac14d306f
Layne Faler <laynefaler@Laynes-MacBook-Pro.local>
2020-06-06 11:12:22 -0700

    Initial file setup

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const path = require('path');
 
 const combineTextFiles = (directoryName, bookName) => {
     // Paths
-    const rootPath = path.join(__dirname, directoryName);
-    const distPath = path.join(__dirname, bookName + '.txt');
+    const rootPath = path.join(__dirname, 'src', directoryName);
+    const distPath = path.join(__dirname, 'lib', bookName + '.txt');
     // File System options
     const options = { encoding: 'utf8' };
     // Initial text file
     fs.writeFileSync(distPath, '', options);
     // Combine text file
     fs.readdirSync(rootPath).forEach(filename => {
-        let filePath = path.join(rootPath, filename);
+        const filePath = path.join(rootPath, filename);
         fs.writeFileSync(distPath, fs.readFileSync(distPath, options) + '\n' + fs.readFileSync(filePath, options));
     });
 };
