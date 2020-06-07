48b314d9879bfb54bc619aac6436e098ea998476
Layne Faler <laynefaler@Laynes-MacBook-Pro.local>
2020-06-06 22:52:04 -0700

    Connected API with FE using React web hooks

--- easy-reader/app/assets/jsx/components/ClientFileReader.jsx
+++ easy-reader/app/assets/jsx/components/ClientFileReader.jsx
@@ -2,20 +2,12 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 
 
-class ClientFileReader extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
-    render() {
-        const { htmlText } = this.props;
-        return (
-            <div className="w-100">
-                <div dangerouslySetInnerHTML={{ __html: htmlText }} />
-            </div>
-        )
-    }
+function ClientFileReader({ htmlText }) {
+    return (
+        <div className="w-100">
+            <div dangerouslySetInnerHTML={{ __html: htmlText }} />
+        </div>
+    )
 }
 
 export default connect(() => ({}))(ClientFileReader);
--- easy-reader/app/assets/jsx/pages/component.jsx
+++ easy-reader/app/assets/jsx/pages/component.jsx
@@ -1,18 +1,47 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 import { connect } from 'react-redux';
-import Header from '../components/Header';
+import { getBookTitles, getFullText } from '../redux/actions/books';
+import { startCase } from 'lodash';
 
 
-class Application extends Component {
-    render() {
-        if (typeof window === 'undefined') return null;
-        
-        return (
-            <div className="app bg-light">
-                <Header />
-            </div>
-        );
+function Application({ dispatch }) {
+    let [book, setBook] = React.useState('');
+    let [titles, setTitles] = React.useState(false);
+    let [effect, setEffect] = React.useState(false);
+
+    async function fetchData() {
+        try {
+            const { data } = await getBookTitles(dispatch);
+            setTitles(data);
+        } catch (error) {
+            setEffect(!effect);
+        }
     }
+
+    async function fetchBookData(file_name) {
+        const response = await getFullText(dispatch, file_name);
+        setBook(response);
+    }
+
+    React.useEffect(() => {
+        fetchData();
+    }, [effect]);
+
+    return (
+        <div className="app bg-light">
+            <div className="w-100 bg-white h-5">
+                {Array.isArray(titles) && titles.map((e, i) => (
+                    <p key={i} className="text-black" onClick={() => fetchBookData(e.file_name)}>
+                        {startCase(e.title)}
+                    </p>
+                ))}
+            </div>
+            <div className="w-100">
+                <div dangerouslySetInnerHTML={{ __html: book }} />
+            </div>
+        </div>
+    );
 };
 
-export default connect(() => ({}))(Application)
+export default connect(() => ({
+}))(Application)
